445e0f34ac83be040f2367af2cfb8079423e3a3c
KateLu666 <KateLuDev@outlook.com>
2023-10-13 14:00:01 -0400

    finish

---
+++ src/components/Todo.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { deleteTodo, toggleTodoCompleted } from "../stores/todoStore";
+  export let todo;
+</script>
+
+<li
+  class="flex items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4"
+>
+  <input
+    name="completed"
+    type="checkbox"
+    checked={todo.completed}
+    on:change={() => toggleTodoCompleted(todo.id)}
+    class="mr-2 form-checkbox h-5 w-5"
+  />
+  <span class={`flex-1 text-gray-800 ${todo.completed ? "line-through" : ""}`}
+    >{todo.text}</span
+  >
+  <button
+    type="button"
+    class="middle none center mr-3 rounded-lg bg-pink-500 py-2 px-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-sm hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    data-ripple-light="true"
+    on:click={() => deleteTodo(todo.id)}
+  >
+    Delete
+  </button>
+</li>
---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { addTodo } from "../stores/todoStore.js";
+  let todo = "";
+  const handleSubmit = () => {
+    addTodo(todo);
+    todo = "";
+    console.log("submitting");
+  };
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+  <div class="flex flex-col text-sm mb-2">
+    <label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+    <input
+      type="text"
+      bind:value={todo}
+      id="todo"
+      name="todo"
+      placeholder="Add a todo"
+      class="appearance-none shadown-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg"
+    />
+  </div>
+  <button
+    type="submit"
+    class="w-full rounded-lg bg-teal-500 py-3 px-6 my-4 font-sans text-xs font-bold uppercase text-white shadow-md shadow-teal-500/20 transition-all hover:shadow-lg hover:shadow-teal-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    data-ripple-light="true"
+  >
+    submit
+  </button>
+</form>
